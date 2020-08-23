d8e9e5d5d601388763a0c07563db0fd438403963
Jesse <nguyenjessev@gmail.com>
2020-08-23 18:13:57 -0400

    Add tree generation

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,10 +1,33 @@
 # frozen-string-literal: true
 
+# This module contains classes and methods related to solving the Knight's Travails problem
 module KnightsTravails
-  # This class represents a knight/board positions (and possible subsequent moves)
+  # This class represents a chess board (in tree form)
+  class Board
+    attr_accessor :root
+
+    def initialize(position)
+      @root = build_tree(position)
+    end
+
+    private
+
+    def build_tree(position, visited = [])
+      current_knight = Knight.new(position)
+      visited << position
+
+      current_knight.legal_moves.each do |move|
+        current_knight.next_moves << build_tree(move, visited) unless visited.include?(move)
+      end
+
+      current_knight
+    end
+  end
+
+  # This class represents a knight (and possible subsequent moves)
   class Knight
-    attr_accessor :x, :y
-    attr_reader :possible_moves, :previous_position
+    attr_accessor :x, :y, :next_moves
+    attr_reader :legal_moves
 
     @@MOVE_VALUES = [[-2, -1],
                      [-2, 1],
@@ -15,21 +38,21 @@ module KnightsTravails
                      [1, -2],
                      [1, 2]]
 
-    def initialize(position, previous_position = [])
+    def initialize(position)
       @x = position[0]
       @y = position[1]
-      @possible_moves = find_moves(previous_position)
+      @legal_moves = find_moves
+      @next_moves = []
     end
 
     private
 
-    def find_moves(previous_position)
+    def find_moves
       moves = []
 
       @@MOVE_VALUES.each do |move|
         new_move = [x + move[0], y + move[1]]
-
-        moves << new_move if valid_move?(new_move) && previous_position != new_move
+        moves << new_move if valid_move?(new_move)
       end
 
       moves
@@ -41,4 +64,6 @@ module KnightsTravails
       false
     end
   end
+
+  def knight_moves(start, target); end
 end
